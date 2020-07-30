3ac085d2ace905088c04373f9ec6eb8d3fca1f53
601121815@qq.com <963.8520cm>
2020-07-30 17:59:27 +0800

    修改样式

--- src/pages/scurryGoodsQry.vue
+++ src/pages/scurryGoodsQry.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="content">
 		<div class="item">
-			<el-form ref="form" :inline="true" style="width:800px" :model="form" label-width="80px">
+			<el-form ref="form" :inline="true" :model="form" label-width="80px">
 				<el-form-item label="起始日期">
 					<el-date-picker
 						v-model="form.value"
@@ -22,26 +22,71 @@
 				<el-form-item label="城市">
 					<el-input v-model="form.username" clearble></el-input>
 				</el-form-item>
-                <el-form-item label="产品" style="width:800px">
+                <el-form-item label="产品名称">
 					<el-input v-model="form.username" clearble></el-input>
 				</el-form-item>
                 <el-form-item label="经销商">
 					<el-input v-model="form.username" clearble></el-input>
 				</el-form-item>
+                <el-form-item label="经销商">
+					<el-button type="primary">查询</el-button>
+				</el-form-item>
 			</el-form>
-			<div class="btn">
-				<el-button type="primary">查询</el-button>
-			</div>
 		</div>
-		<div class="item" v-if="tableData.length > 0">
+		<div class="item">
 			<el-table :data="tableData" border>
-				<el-table-column label="操作时间"></el-table-column>
-				<el-table-column label="用户名"></el-table-column>
-				<el-table-column label="操作类型"></el-table-column>
-				<el-table-column label="信息系"></el-table-column>
-				<el-table-column label="旧信息"></el-table-column>
+				<el-table-column label="防伪码"></el-table-column>
+				<el-table-column label="物流码"></el-table-column>
+				<el-table-column label="批次"></el-table-column>
+				<el-table-column label="产品名称"></el-table-column>
+				<el-table-column label="查询省份"></el-table-column>
+				<el-table-column label="查询城市"></el-table-column>
+				<el-table-column label="查询区域"></el-table-column>
+				<el-table-column label="经销商"></el-table-column>
+				<el-table-column label="首次查询时间"></el-table-column>
+				<el-table-column label="查询方式"></el-table-column>
+				<el-table-column label="查询信息"></el-table-column>
+				<el-table-column label="查看物流明细">
+					<template>
+						<el-button type="primary" @click="dialogVisible = true;">查询</el-button>
+					</template>
+				</el-table-column>
 			</el-table>
+			<el-pagination
+				background
+				@current-change="handleCurrentChange"
+				layout="prev, pager, next"
+				:total="100">
+			</el-pagination>
 		</div>
+		<el-dialog
+			title="物流明细"
+			:visible.sync="dialogVisible"
+			width="40%">
+			<el-table :data="tableData" border>
+				<el-table-column label="防伪码"></el-table-column>
+				<el-table-column label="物流码"></el-table-column>
+				<el-table-column label="操作时间"></el-table-column>
+				<el-table-column label="产品名称"></el-table-column>
+				<el-table-column label="操作项目"></el-table-column>
+				<el-table-column label="单号"></el-table-column>
+				<el-table-column label="调出库房"></el-table-column>
+				<el-table-column label="调入库房"></el-table-column>
+				<el-table-column label="调出经销商"></el-table-column>
+				<el-table-column label="调入经销商"></el-table-column>
+				<el-table-column label="操作员"></el-table-column>
+			</el-table>
+			<el-pagination
+				background
+				@current-change="handleCurrentChange"
+				layout="prev, pager, next"
+				:total="100">
+			</el-pagination>
+			<span slot="footer" class="dialog-footer">
+				<el-button @click="dialogVisible = false">取 消</el-button>
+				<el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+			</span>
+		</el-dialog>
 	</div>
 </template>
 
@@ -49,6 +94,7 @@
 export default {
 	data(){
 		return {
+			dialogVisible: false,
 			form: {
 				username: '',
 				name: '',
@@ -61,9 +107,16 @@ export default {
 				value: '选项2',
 				label: '双皮奶'
 			}],
-			tableData: []
+			tableData: [
+				{}
+			]
 		}
-	}
+	},
+	methods: {
+		handleCurrentChange(val) {
+			console.log(`当前页: ${val}`);
+		}
+	},
 }
 </script>
 
