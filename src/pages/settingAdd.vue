57e79c3b9f85c914f6a5fd9e4da12f3449ef2b1c
chengmo <601121815@qq.com>
2020-09-12 17:41:35 +0800

    修改

--- src/pages/settingAdd.vue
+++ src/pages/settingAdd.vue
@@ -1,64 +1,222 @@
 <template>
-	<div class="content">
-		<div class="item">
-			<el-form ref="form" style="width:500px" :model="form" label-width="80px">
-				<el-form-item label="用户名">
-					<el-input v-model="form.username"></el-input>
-				</el-form-item>
-				<el-form-item label="真实姓名">
-					<el-input v-model="form.name"></el-input>
-				</el-form-item>
-			</el-form>
-			<div class="btn">
-				<el-button type="primary">查询</el-button>
-				<el-button type="primary">添加</el-button>
-			</div>
-		</div>
-		<div class="item" v-if="tableData.length > 0">
-			<el-table :data="tableData" border>
-				<el-table-column label="用户名"></el-table-column>
-				<el-table-column label="真实姓名"></el-table-column>
-				<el-table-column label="性别"></el-table-column>
-				<el-table-column label="联系电话"></el-table-column>
-				<el-table-column label="所属部门"></el-table-column>
-				<el-table-column label="账号类型"></el-table-column>
-				<el-table-column label="负责经销商"></el-table-column>
-				<el-table-column label="启用状态"></el-table-column>
-				<el-table-column label="操作">
-					<template>
-						<el-button type="text">添加</el-button>
-					</template>
-				</el-table-column>
-			</el-table>
-		</div>
-	</div>
+  <div class="content">
+    <div class="item">
+      <div class="btn">
+        <el-button icon="el-icon-plus" type="primary" @click="AddProduct">添加</el-button>
+        <el-button icon="el-icon-search" type="primary">查询</el-button>
+        <el-button type="primary">导出</el-button>
+      </div>
+      <el-form ref="form" :inline="true" :model="form" label-width="100px">
+        <el-form-item label="经销商名称">
+          <el-input v-model="form.username" clearble></el-input>
+        </el-form-item>
+        <el-form-item label="经销商编号">
+          <el-input v-model="form.username" clearble></el-input>
+        </el-form-item>
+        <el-form-item label="省份">
+          <el-select v-model="form.value" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="城市">
+          <el-select v-model="form.value" placeholder="请选择">
+            <el-option
+              v-for="item in state"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="区域">
+          <el-select v-model="form.value" placeholder="请选择">
+            <el-option
+              v-for="item in state"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div class="item">
+      <el-table :data="tableData" border>
+        <el-table-column align="center" label="经销商编号"></el-table-column>
+        <el-table-column align="center" label="经销商名称"></el-table-column>
+        <el-table-column align="center" label="级别"></el-table-column>
+        <el-table-column align="center" label="所在省份"></el-table-column>
+        <el-table-column align="center" label="城市"></el-table-column>
+        <el-table-column align="center" label="区域"></el-table-column>
+        <el-table-column align="center" label="状态"></el-table-column>
+        <el-table-column align="center" label="操作">
+          <template>
+            <el-button type="text">修改</el-button>
+            <el-button type="text">禁用</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-dialog :title="dialogtype==1 ? '添加经销商':'编辑经销商'" :visible.sync="dialogVisible" width="740px">
+        <el-form
+          ref="form"
+          inline
+          style="width:auto"
+          :model="form"
+          :rules="rules"
+          label-width="100px"
+        >
+          <el-form-item label="经销商名称" prop="ModuleName">
+            <el-input v-model="form.ModuleName"></el-input>
+          </el-form-item>
+          <el-form-item label="经销商编号" prop="ModuleId">
+            <el-input v-model="form.ModuleId"></el-input>
+          </el-form-item>
+          <el-form-item label="省份" prop="WebPageUrl">
+            <el-select v-model="form.value" placeholder="请选择">
+				<el-option
+					v-for="item in state"
+					:key="item.value"
+					:label="item.label"
+					:value="item.value"
+					></el-option>
+			</el-select>
+          </el-form-item>
+          <el-form-item label="城市" prop="WebPageUrl">
+            <el-input v-model="form.WebPageUrl"></el-input>
+          </el-form-item>
+          <el-form-item label="区域" prop="WebPageUrl">
+            <el-input v-model="form.WebPageUrl"></el-input>
+          </el-form-item>
+          <el-form-item label="负责区域" prop="SeqNo">
+            <el-radio v-model="form.SeqNo">仅本省</el-radio>
+            <el-radio v-model="form.SeqNo">仅本市</el-radio>
+            <el-radio v-model="form.SeqNo">自定义</el-radio>
+          </el-form-item>
+          <el-form-item label="经销商级别" prop="WebPageUrl">
+            <el-select v-model="form.value" placeholder="请选择">
+				<el-option
+					v-for="item in state"
+					:key="item.value"
+					:label="item.label"
+					:value="item.value"
+					></el-option>
+			</el-select>
+          </el-form-item>
+          <el-form-item label="上级经销商" prop="WebPageUrl">
+            <el-select v-model="form.value" placeholder="请选择">
+				<el-option
+					v-for="item in state"
+					:key="item.value"
+					:label="item.label"
+					:value="item.value"
+					></el-option>
+			</el-select>
+          </el-form-item>
+          <el-form-item label="经销商类型" prop="WebPageUrl">
+            <el-select v-model="form.value" placeholder="请选择">
+				<el-option
+					v-for="item in state"
+					:key="item.value"
+					:label="item.label"
+					:value="item.value"
+					></el-option>
+			</el-select>
+          </el-form-item>
+          <el-form-item label="详细地址" prop="WebPageUrl">
+			  <el-input v-model="form.value" placeholder="详细地址"></el-input>
+          </el-form-item>
+          <el-form-item label="联系人" prop="WebPageUrl">
+			  <el-input v-model="form.value" placeholder="联系人"></el-input>
+          </el-form-item>
+          <el-form-item label="联系电话" prop="WebPageUrl">
+			  <el-input v-model="form.value" placeholder="联系电话"></el-input>
+          </el-form-item>
+          <el-form-item label="备注" prop="WebPageUrl">
+			  <el-input v-model="form.value" placeholder="备注"></el-input>
+          </el-form-item>
+          <el-form-item label="状态" prop="WebPageUrl">
+			  <el-radio v-model="form.value" :label="1">启用</el-radio>
+			  <el-radio v-model="form.value" :label="0">禁用</el-radio>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="restForm">重 置</el-button>
+          <el-button type="primary" @click="SetProduct">确 定</el-button>
+        </span>
+      </el-dialog>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	data(){
-		return {
-			form: {
-				username: '',
-				name: ''
-			},
-			tableData: []
-		}
-	}
-}
+  data() {
+    return {
+      form: {
+        username: "",
+        name: "",
+        value: null,
+      },
+      state: [
+        {
+          value: 0,
+          label: "禁用",
+        },
+        {
+          value: 1,
+          label: "启用",
+        },
+      ],
+      tableData: [],
+      dialogVisible: false,
+      dialogtype: 0,
+      dialogPack: false,
+    };
+  },
+  methods: {
+    AddProduct() {
+      this.dialogVisible = true;
+      this.dialogtype = 1;
+    },
+    restForm() {
+      this.$nextTick(() => {
+        this.$refs["form"].resetFields();
+      });
+    },
+    SetProduct() {
+      this.$refs["form"].validate((isValidate) => {
+        if (isValidate) {
+        } else {
+        }
+      });
+    },
+    reback() {
+      this.dialogPack = false;
+    },
+    setPack() {},
+  },
+};
 </script>
 
 <style scoped>
-.title{
+.title {
   text-align: left;
 }
-.item{
-	margin: 20px 0;
-	padding: 20px;
-	border: 1px solid #e3e3e3;
+.item {
+  margin: 10px 0;
+  border: 1px solid #e3e3e3;
 }
-.btn{
-	width: 500px;
-	margin:0 80px;
+.btn {
+  width: 500px;
+  margin: 10px 80px;
+}
+.el-input,
+.el-select {
+  width: 220px;
 }
 </style>
--- src/pages/settingPacking.vue
+++ src/pages/settingPacking.vue
@@ -1,71 +1,127 @@
 <template>
-	<div class="content">
-		<div class="item">
-			<el-form ref="form" :inline="true" style="width:800px" :model="form" label-width="80px">
-				<el-form-item label="城市">
-					<el-select v-model="form.value" placeholder="请选择">
-						<el-option
-						v-for="item in options"
-						:key="item.value"
-						:label="item.label"
-						:value="item.value">
-						</el-option>
-					</el-select>
-				</el-form-item>
-			</el-form>
-			<div class="btn">
-				<el-button type="primary">查询</el-button>
-				<el-button type="primary">添加</el-button>
-				<el-button type="primary">导出</el-button>
-			</div>
-		</div>
-		<div class="item" v-if="tableData.length > 0">
-			<el-table :data="tableData" border>
-				<el-table-column label="包装层级"></el-table-column>
-				<el-table-column label="比例表达式"></el-table-column>
-				<el-table-column label="操作">
-					<template>
-						<el-button type="text">添加</el-button>
-					</template>
-				</el-table-column>
-			</el-table>
-		</div>
-	</div>
+  <div class="content">
+    <div class="item">
+      <div class="btn">
+        <el-button icon="el-icon-plus" type="primary" @click="AddProduct">添加</el-button>
+        <el-button icon="el-icon-search" type="primary">查询</el-button>
+        <el-button type="primary">导出</el-button>
+      </div>
+      <el-form ref="form" :inline="true" :model="form" label-width="100px">
+        <el-form-item label="包装层级">
+          <el-select v-model="form.value" placeholder="请选择">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div class="item">
+      <el-table :data="tableData" border>
+        <el-table-column align="center" label="包装层级"></el-table-column>
+        <el-table-column align="center" label="比例表达式"></el-table-column>
+        <el-table-column align="center" label="操作">
+          <template>
+            <el-button type="text">修改</el-button>
+            <el-button type="text">禁用</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-dialog title="添加包装比例" :visible.sync="dialogPack" width="360px">
+        <el-form ref="form" style="width:auto" :model="form" label-width="100px">
+          <el-form-item label="包装层级">
+            <el-input v-model="form.ModuleId"></el-input>
+          </el-form-item>
+          <el-form-item label="三级包装数量">
+            <el-input v-model="form.ModuleId"></el-input>
+          </el-form-item>
+          <el-form-item label="二级包装数量">
+            <el-input v-model="form.ModuleId"></el-input>
+          </el-form-item>
+          <el-form-item label="一级包装数量">
+            <el-input v-model="form.ModuleId"></el-input>
+          </el-form-item>
+          <el-form-item label="比例表单式">
+            <el-input v-model="form.ModuleId"></el-input>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="reback">返 回</el-button>
+          <el-button type="primary" @click="SetPack">提 交</el-button>
+        </span>
+      </el-dialog>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-	data(){
-		return {
-			form: {
-				username: '',
-				name: '',
-				value: null
-			},
-			options: [{
-				value: '选项1',
-				label: '黄金糕'
-				}, {
-				value: '选项2',
-				label: '双皮奶'
-			}],
-			tableData: []
-		}
-	}
-}
+  data() {
+    return {
+      form: {
+        username: "",
+        name: "",
+		value: null,
+		ModuleId: null
+	  },
+	  options: [],
+      state: [
+        {
+          value: 0,
+          label: "禁用",
+        },
+        {
+          value: 1,
+          label: "启用",
+        },
+      ],
+      tableData: [],
+      dialogtype: 0,
+      dialogPack: false,
+    };
+  },
+  methods: {
+    AddProduct() {
+      this.dialogPack = true;
+      this.dialogtype = 1;
+    },
+    restForm() {
+      this.$nextTick(() => {
+        this.$refs["form"].resetFields();
+      });
+    },
+    SetProduct() {
+      this.$refs["form"].validate((isValidate) => {
+        if (isValidate) {
+        } else {
+        }
+      });
+    },
+    reback() {
+      this.dialogPack = false;
+    },
+    SetPack() {},
+  },
+};
 </script>
 
 <style scoped>
-.title{
+.title {
   text-align: left;
 }
-.item{
-	margin: 20px 0;
-	padding: 20px;
-	border: 1px solid #e3e3e3;
+.item {
+  margin: 10px 0;
+  border: 1px solid #e3e3e3;
 }
-.btn{
-	width: 500px;
-	margin:0 80px;
+.btn {
+  width: 500px;
+  margin: 10px 80px;
+}
+.el-input,
+.el-select {
+  width: 220px;
 }
 </style>
